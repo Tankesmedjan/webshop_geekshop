c7307fcb503aa3e67d0c1aeb6145fe2d70aebce7
ddewe <ddewee92@gmail.com>
2021-03-29 16:35:46 +0200

    Added BrandsService

---
+++ src/main/java/tankesmedjan/webshop/Service/BrandsService.java
@@ -0,0 +1,34 @@
+package tankesmedjan.webshop.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tankesmedjan.webshop.models.Brands;
+import tankesmedjan.webshop.models.Products;
+import tankesmedjan.webshop.repos.BrandsRepo;
+import tankesmedjan.webshop.repos.ProductsRepo;
+
+import java.util.List;
+
+@Service
+public class BrandsService {
+
+    @Autowired
+    private BrandsRepo brandsRepo;
+
+    @Autowired
+    ProductsRepo productsRepo;
+
+    public Brands saveBrand(Brands brands) {
+        brandsRepo.save(brands);
+        return brands;
+    }
+
+    public List<Brands> findAll() {
+        return brandsRepo.findAll();
+    }
+
+    public List<Products> getProductsByBrandId(Long id) {
+        return productsRepo.findAllByBrands_Id(id);
+    }
+
+}
--- src/main/java/tankesmedjan/webshop/controllers/BrandsController.java
+++ src/main/java/tankesmedjan/webshop/controllers/BrandsController.java
@@ -1,10 +1,9 @@
 package tankesmedjan.webshop.controllers;
 
 import org.springframework.web.bind.annotation.*;
+import tankesmedjan.webshop.Service.BrandsService;
 import tankesmedjan.webshop.models.Brands;
-import tankesmedjan.webshop.models.Categories;
 import tankesmedjan.webshop.models.Products;
-import tankesmedjan.webshop.repos.BrandsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import tankesmedjan.webshop.repos.ProductsRepo;
 
@@ -16,24 +15,21 @@ import java.util.List;
 public class BrandsController {
 
     @Autowired
-    private BrandsRepo brandsRepo;
-
-    @Autowired
-    private ProductsRepo productsRepo;
+    private BrandsService brandsService;
 
     @GetMapping
     public List<Brands> getAllBrands() {
-        return brandsRepo.findAll();
+        return brandsService.findAll();
     }
 
     @PostMapping
     public Brands addBrand(@RequestBody Brands brand) {
-        brandsRepo.save(brand);
+        brandsService.saveBrand(brand);
         return brand;
     }
 
     @GetMapping("/{id}")
-    public List<Products> getAllProductsFomBrand(@PathVariable Long id) {
-        return productsRepo.findAllByBrands_Id(id);
+    public List<Products> getAllProductsFromBrand(@PathVariable Long id) {
+        return brandsService.getProductsByBrandId(id);
     }
 }
