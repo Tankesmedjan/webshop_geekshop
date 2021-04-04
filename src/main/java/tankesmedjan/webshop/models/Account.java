503d5bee0cb76e96b6b6bd56df72c71d5291b715
Michael <michael@graphicdesire.se>
2021-04-04 16:32:39 +0200

    sha256 of password

--- src/main/java/tankesmedjan/webshop/models/Account.java
+++ src/main/java/tankesmedjan/webshop/models/Account.java
@@ -1,14 +1,18 @@
 package tankesmedjan.webshop.models;
 
 
+import com.google.common.hash.Hashing;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 import tankesmedjan.webshop.dto.AccountCreationDTO;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.nio.charset.StandardCharsets;
+
 
 @Entity
 @Getter
@@ -18,14 +22,19 @@ public class Account {
 
     public Account(AccountCreationDTO accounts) {
 
+        String sha256hex = Hashing.sha256()
+                .hashString(accounts.getPassword(), StandardCharsets.UTF_8)
+                .toString();
+
         this.username = accounts.getUsername();
-        this.password = accounts.getPassword();
+        this.password = sha256hex;
         this.role = accounts.getRole();
     }
 
     @Id
-    @GeneratedValue
-    private long id;
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
 
     private String username;
     private String password;
