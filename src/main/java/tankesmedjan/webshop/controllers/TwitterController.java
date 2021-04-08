aa547a8c0a21b895f1e08f17b7e4e31f120e7440
Richardsonn1 <marcus.richardson@hotmail.se>
2021-04-08 15:46:56 +0200

    Twtittercontroller updated and twitter dto added.

--- src/main/java/tankesmedjan/webshop/controllers/TwitterController.java
+++ src/main/java/tankesmedjan/webshop/controllers/TwitterController.java
@@ -2,6 +2,7 @@ package tankesmedjan.webshop.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tankesmedjan.webshop.dto.TwitterDTO;
 import tankesmedjan.webshop.services.TwitterService;
 import twitter4j.TwitterException;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @RequestMapping(path = "/twitter")
 public class TwitterController {
 
-    private TwitterService twitterService;
+    private final TwitterService twitterService;
 
     @Autowired
     public TwitterController(TwitterService twitterService){
@@ -24,8 +25,10 @@ public class TwitterController {
     }
 
     @PostMapping(path = "/post-to-twitter")
-    public String postToTwitter(@RequestParam String message, @RequestParam Integer id) throws TwitterException {
-        twitterService.postToTwitter(message + twitterService.getHashtags().get(id));
-        return message + " " + twitterService.getHashtags().get(id);
+    public String postToTwitter(@RequestBody TwitterDTO twitterDTO) throws TwitterException {
+        twitterService.postToTwitter(String.format("%-" + (twitterDTO.getMessage().length() + 1) + "s", twitterDTO.getMessage()) + String.format("%-" + (twitterDTO.getUrl().length() + 1) + "s", twitterDTO.getUrl()) +
+                twitterService.getHashtags().get(twitterDTO.getHashtag()));
+        return String.format("%-" + (twitterDTO.getMessage().length() + 1) + "s", twitterDTO.getMessage()) + String.format("%-" + (twitterDTO.getUrl().length() + 1) + "s", twitterDTO.getUrl()) +
+                twitterService.getHashtags().get(twitterDTO.getHashtag());
     }
 }
---
+++ src/main/java/tankesmedjan/webshop/dto/TwitterDTO.java
@@ -0,0 +1,18 @@
+package tankesmedjan.webshop.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TwitterDTO {
+
+    private String message;
+    private String url;
+    private int hashtag;
+
+
+    //    private ImageIcon img;
+
+}
