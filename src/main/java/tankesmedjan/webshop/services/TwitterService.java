ea6aca7720534810f4b93e89302747e712ef696d
Richardsonn1 <marcus.richardson@hotmail.se>
2021-04-14 12:59:06 +0200

    TwitterService updated with documentation.

--- src/main/java/tankesmedjan/webshop/services/TwitterService.java
+++ src/main/java/tankesmedjan/webshop/services/TwitterService.java
@@ -5,7 +5,6 @@ import org.springframework.transaction.annotation.Transactional;
 import tankesmedjan.webshop.dto.HashtagDTO;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
-
 import java.util.*;
 import java.util.stream.*;
 
@@ -13,27 +12,33 @@ import java.util.stream.*;
 @Transactional
 public class TwitterService {
 
-    Logger logger = Logger.getLogger(TwitterService.class);
-
+    /**
+     * twitter.getPlaceTrends access the trending trends at a certain area(continent, country, state, city).
+     * The stream streams the array of trending trends, maps the trend name into a object<HashtagDTO> and then
+     * filter the stream to only preserve the trends containing a hashtag. Converts the stream into a list at the end.
+     * @return a List<HashtagDTO> with trending hashtags in the UK.
+     */
     public List<HashtagDTO> getHashtags() throws TwitterException {
-
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true);
         TwitterFactory tf = new TwitterFactory(cb.build());
         Twitter twitter = tf.getInstance();
         Trends trends = twitter.getPlaceTrends(23424975);
 
-        var hash = Arrays.stream(trends.getTrends())
+        return Arrays.stream(trends.getTrends())
                 .map(trend -> new HashtagDTO(trend.getName()))
                 .filter(h -> h.hashtagName.contains("#"))
                 .collect(Collectors.toList());
-        return hash;
     }
 
+    /**
+     * twitter.updateStatus() updates the status of the twitter account.
+     * @param message contains the users input of a message, url and hashtag.
+     * @return the input from the user with linebreaks.
+     */
     public String postToTwitter(String message) throws TwitterException {
         Twitter twitter = TwitterFactory.getSingleton();
-        Status status = twitter.updateStatus(message);
-        logger.debug("Successfully updated status to " + status.getText());
+        twitter.updateStatus(message);
         return message;
     }
 }
