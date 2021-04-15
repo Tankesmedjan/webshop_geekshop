9174f7bf63559159774a2082922689f8a1447bba
Michael <michael@graphicdesire.se>
2021-04-15 10:49:35 +0200

    demo-dump.sql updated

--- root_files/demo-dump.sql
+++ root_files/demo-dump.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 5.7.30, for osx10.12 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.26, for osx10.10 (x86_64)
 --
 -- Host: 151.177.147.211    Database: demo
 -- ------------------------------------------------------
@@ -37,7 +37,7 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES ('dab06b40-9d5f-454d-86d4-2d9ab6bbf4ea','c592df4a86933b92addc9842402ddf198c638ea9be58916ee6e3734e1e3152f8','User','BasicUser'),('c5afbd9e-1010-4995-a489-7f419e1d8bd1','93915a0a4bf8f634cb1856494dd4304472ad46b9827f541f76b6761c49cc55b0','Admin','Admin'),('675c6dc3-ad44-4a31-9842-9db4fdbd3667','17b33d80470fb3fff29eb048d6bcce03ba14e305c4851baf39ac3d37a183c419','User','BosseBil');
+INSERT INTO `account` VALUES ('dab06b40-9d5f-454d-86d4-2d9ab6bbf4ea','c592df4a86933b92addc9842402ddf198c638ea9be58916ee6e3734e1e3152f8','User','BasicUser'),('c5afbd9e-1010-4995-a489-7f419e1d8bd1','93915a0a4bf8f634cb1856494dd4304472ad46b9827f541f76b6761c49cc55b0','Admin','Admin'),('675c6dc3-ad44-4a31-9842-9db4fdbd3667','17b33d80470fb3fff29eb048d6bcce03ba14e305c4851baf39ac3d37a183c419','User','BosseBil'),('e351a7fd-76d1-4d9f-b1d5-ce8c085b2184','f2d01425ea08d15fb5ec07b40eb698dcd7d3ca4ecffa8091b0f50edb0eb29d31','User','Bengan'),('3d05919a-9fdc-4774-99d4-e7780aeda281','9e6d5007769f9ea11a5ed587d4f05e8e5e1385453b117a0165d1e09fd1b407ab','Admin','hejmi');
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,7 +52,7 @@ CREATE TABLE `attribute` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `attribute_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -78,7 +78,7 @@ CREATE TABLE `attribute_option` (
   `attribute` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FKau3wlkygar2s0fn4s9cku3e1k` (`attribute`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -87,7 +87,7 @@ CREATE TABLE `attribute_option` (
 
 LOCK TABLES `attribute_option` WRITE;
 /*!40000 ALTER TABLE `attribute_option` DISABLE KEYS */;
-INSERT INTO `attribute_option` VALUES (1,'Red',1),(2,'Green',1),(3,'White',1),(4,'Black',1),(5,'Blue',1),(6,'Pink',1),(7,'Yellow',1),(8,'New',3),(9,'Used, good condition',3),(10,'Used, ok condition',3),(11,'Used, bad condition',3),(12,'XS',2),(13,'S',2),(14,'M',2),(15,'L',2),(16,'XL',2),(17,'XXL',2),(18,'Purple',1),(0,'None',0);
+INSERT INTO `attribute_option` VALUES (1,'Red',1),(2,'Green',1),(3,'White',1),(4,'Black',1),(5,'Blue',1),(6,'Pink',1),(7,'Yellow',1),(8,'New',3),(9,'Used, good condition',3),(10,'Used, ok condition',3),(11,'Used, bad condition',3),(12,'XS',2),(13,'S',2),(14,'M',2),(15,'L',2),(16,'XL',2),(17,'XXL',2),(18,'Purple',1),(0,'None',0),(20,'Teal',1);
 /*!40000 ALTER TABLE `attribute_option` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,7 +103,7 @@ CREATE TABLE `brand` (
   `brand_name` varchar(32) DEFAULT NULL,
   `brand_logo` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,7 +128,7 @@ CREATE TABLE `category` (
   `category_name` varchar(32) DEFAULT NULL,
   `parent_category_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -160,7 +160,7 @@ CREATE TABLE `customer` (
   `account_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FKn9x2k8svpxj3r328iy1rpur83` (`account_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -169,7 +169,7 @@ CREATE TABLE `customer` (
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES (1,'Test Street 1','New York','thegeeksqueek@gmail.com','Basic','User','1-800-555-123456','100 90','dab06b40-9d5f-454d-86d4-2d9ab6bbf4ea'),(3,'verkstadsvägen 1','porsche','bildoktorn@fixar.com','Bosse','Bildoktorn','0731121122','911 11','675c6dc3-ad44-4a31-9842-9db4fdbd3667'),(4,'Unke','Unke','admin@unke.se','Admin','Kalle','123456789','123 45','c5afbd9e-1010-4995-a489-7f419e1d8bd1');
+INSERT INTO `customer` VALUES (1,'Test Street 1','New York','thegeeksqueek@gmail.com','Basic','User','1-800-555-123456','100 90','dab06b40-9d5f-454d-86d4-2d9ab6bbf4ea'),(3,'verkstadsvägen 1','porsche','bildoktorn@fixar.com','Bosse','Bildoktorn','0731121122','911 11','675c6dc3-ad44-4a31-9842-9db4fdbd3667'),(4,'Unke','Unke','admin@unke.se','Admin','Kalle','123456789','123 45','c5afbd9e-1010-4995-a489-7f419e1d8bd1'),(10,'Småstugevägen 21','Göteborg','michael@graphicdesire.se','Michael','Hejl','0735103910','416 76','3d05919a-9fdc-4774-99d4-e7780aeda281'),(12,'bengangatan','göteborg','bengan@beng.beng','Bengt','Bengtson','12341234','123 12','e351a7fd-76d1-4d9f-b1d5-ce8c085b2184');
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -226,28 +226,6 @@ LOCK TABLES `frontpage_slide` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `hashtag`
---
-
-DROP TABLE IF EXISTS `hashtag`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `hashtag` (
-  `hashtag` varchar(100) NOT NULL,
-  PRIMARY KEY (`hashtag`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `hashtag`
---
-
-LOCK TABLES `hashtag` WRITE;
-/*!40000 ALTER TABLE `hashtag` DISABLE KEYS */;
-/*!40000 ALTER TABLE `hashtag` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `hibernate_sequence`
 --
 
@@ -291,7 +269,7 @@ CREATE TABLE `product` (
   KEY `FKs6cydsualtsrprvlf2bb3lcam` (`brand_id`),
   KEY `FKq6hhjex8p6noh29xumcr247oc` (`catid`),
   KEY `FKj7prsct7jf0yweq69tdnxqgnw` (`vat_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=47 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -300,7 +278,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (1,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.40',2.99,'Aquaman Comics',15,0,0,3,1),(2,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.41',2.99,'Aquaman Comics',15,1,1,3,1),(3,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.42',2.99,'Aquaman Comics',15,0,0,3,1),(4,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.43',2.99,'Aquaman Comics',15,1,1,3,1),(5,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.44',2.99,'Aquaman Comics',15,0,0,3,1),(6,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.45',2.99,'Aquaman Comics',15,0,1,3,1),(7,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.46',2.99,'Aquaman Comics',15,1,1,3,1),(12,'Pokemon Pika Pop T-shirt','Pokemon T-shirt',24.99,'Pokemon T-shirt',10,1,1,8,1),(20,'Star Wars T-shirt with Yoda motive','Yoda t-shirt',25.99,'Star Wars t-shirt',10,0,1,7,1),(33,'Star Wars T-shirt with Darth Vader motive','Darth Vader t-shirt',24.99,'Star Wars t-shirt',10,0,1,7,1),(40,'Dominus Dudes is a 4 pieces set of popular Roblox action figures.','Dominus Dudes',24.99,'Set with 4 Roblox figures',8,0,1,1,1);
+INSERT INTO `product` VALUES (1,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.40',2.99,'Aquaman Comics',15,0,0,3,1),(2,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.41',4.99,'Aquaman Comics',15,0,1,3,1),(3,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.42',2.99,'Aquaman Comics',15,0,0,3,1),(4,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.43',2.99,'Aquaman Comics',15,1,1,3,1),(5,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.44',2.99,'Aquaman Comics',15,0,0,3,1),(6,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.45',2.99,'Aquaman Comics',15,0,1,3,1),(7,'(W) Kelly Sue DeConnick (A/CA) Robson Rocha, Daniel Henriques','Aquaman No.46',2.99,'Aquaman Comics',15,1,1,3,1),(12,'Pokemon Pika Pop T-shirt','Pokemon T-shirt',24.99,'Pokemon T-shirt',10,1,1,8,1),(20,'Star Wars T-shirt with Yoda motive','Yoda t-shirt',25.99,'Star Wars t-shirt',10,0,1,7,1),(33,'Star Wars T-shirt with Darth Vader motive','Darth Vader t-shirt',24.99,'Star Wars t-shirt',10,0,1,7,1),(40,'Dominus Dudes is a 4 pieces set of popular Roblox action figures.','Dominus Figures',29.99,'Set with 4 Roblox figures',8,1,1,1,1);
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -313,17 +291,14 @@ DROP TABLE IF EXISTS `product_rating`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `product_rating` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `rating` int(11) DEFAULT NULL,
-  `ratingmessage` longtext,
-  `account_id` varchar(255) DEFAULT NULL,
-  `products_id` bigint(20) DEFAULT NULL,
   `rating_message` longtext,
   `product_id` bigint(20) DEFAULT NULL,
+  `customer_id` bigint(20) DEFAULT NULL,
+  `rating` double DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK4pwi5t39aa0p08cqfsp5yh8qi` (`products_id`),
-  KEY `FKihfmkgwbxme220v7olcraxkjm` (`account_id`),
+  KEY `FKr3bv2mef1lul80ijj1cieq9mw` (`customer_id`),
   KEY `FKt3mecsgki7hdg8srke4baeidk` (`product_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -332,7 +307,7 @@ CREATE TABLE `product_rating` (
 
 LOCK TABLES `product_rating` WRITE;
 /*!40000 ALTER TABLE `product_rating` DISABLE KEYS */;
-INSERT INTO `product_rating` VALUES (3,5,'Great quality and awesome motive','dab06b40-9d5f-454d-86d4-2d9ab6bbf4ea',33,NULL,33);
+INSERT INTO `product_rating` VALUES (4,'Great quality and awesome motive',20,3,4),(5,'Great',20,2,2.5),(6,'Good',40,3,4);
 /*!40000 ALTER TABLE `product_rating` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -354,7 +329,7 @@ CREATE TABLE `sku` (
   UNIQUE KEY `UK_j5vwm8w70olis07rsnjeeyvka` (`sku`),
   KEY `FKixafovqfh0bn0kakqk45wfi4i` (`attribute_option_id`),
   KEY `FKovd96qvc9da3k94gf0j822p3o` (`product_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=56 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=71 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -363,7 +338,7 @@ CREATE TABLE `sku` (
 
 LOCK TABLES `sku` WRITE;
 /*!40000 ALTER TABLE `sku` DISABLE KEYS */;
-INSERT INTO `sku` VALUES (1,NULL,'111-111-40',1,1,0),(2,NULL,'111-111-41',1,2,0),(3,NULL,'111-111-42',1,3,0),(4,NULL,'111-111-43',1,4,0),(5,NULL,'111-111-44',1,5,0),(6,NULL,'111-111-45',1,6,0),(7,NULL,'111-111-46',1,7,0),(8,NULL,'111-111-47-1',10,12,13),(9,NULL,'111-111-47-2',10,12,14),(10,NULL,'111-111-47-3',10,12,15),(24,NULL,'111-111-48-1',10,20,15),(25,NULL,'111-111-48-2',10,20,16),(26,NULL,'111-111-48-3',10,20,17),(43,NULL,'111-111-50',10,33,0),(49,NULL,'222-222-42',5,40,8),(54,NULL,'222-222-43',2,40,9);
+INSERT INTO `sku` VALUES (1,NULL,'111-111-40',1,1,0),(2,NULL,'111-111-41',1,2,0),(3,NULL,'111-111-42',1,3,0),(4,NULL,'111-111-43',1,4,0),(5,NULL,'111-111-44',1,5,0),(6,NULL,'111-111-45',1,6,0),(7,NULL,'111-111-46',1,7,0),(8,NULL,'111-111-47-1',10,12,13),(9,NULL,'111-111-47-2',10,12,14),(10,NULL,'111-111-47-3',10,12,15),(24,NULL,'111-111-48-1',10,20,15),(25,NULL,'111-111-48-2',10,20,16),(26,NULL,'111-111-48-3',10,20,17),(43,NULL,'111-111-50',10,33,0),(49,NULL,'222-222-42',5,40,8);
 /*!40000 ALTER TABLE `sku` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -401,4 +376,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-11 20:29:52
+-- Dump completed on 2021-04-15 10:49:10
